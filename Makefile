4007622b404960cbd0c992f285f9b389e1e86979
Galen Guyer <galen@galenguyer.com>
2021-02-05 00:47:20 -0500

    basic multithreading

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 brute: md5.c brute.c
-	gcc -std=c99 -Wall brute.c md5.c -O2 -o brute
+	gcc -std=c99 -Wall -pthread brute.c md5.c -O2 -o brute
 
 debug: md5.c brute.c
-	gcc -ggdb -std=c99 -Wall brute.c md5.c -o brute
+	gcc -ggdb -std=c99 -pthread -Wall brute.c md5.c -o brute
--- brute.c
+++ brute.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
+#include <stdint.h>
+#include <pthread.h>
 #include <sys/sysinfo.h>
 
+void* brute(void* thread_id) {
+    printf("[%ld] Starting thread %ld\n", (intptr_t)thread_id, (intptr_t)thread_id);
+    
+    pthread_exit(NULL);
+}
+
 int main() {
-    printf("[INFO] Running on %d threads\n", get_nprocs());
+    int cpu_count = get_nprocs();
+    printf("[INFO] Running on %d threads\n", cpu_count);
+    pthread_t threads[cpu_count];
+    for (intptr_t i = 0; i < cpu_count; i++) {
+        pthread_create(&threads[i], NULL, brute, (void*)i);
+    }
+    for (intptr_t i = 0; i < cpu_count; i++) {
+        pthread_join(&threads[i], NULL);
+    }
     return 0;
 }
