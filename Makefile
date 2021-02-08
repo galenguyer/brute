68888bec95fc3e5a8e94d3700aad0b30b7332472
Galen Guyer <galen@galenguyer.com>
2021-02-08 12:25:12 -0500

    Add basic benchmarking

--- Makefile
+++ Makefile
@@ -1,5 +1,8 @@
 brute: md5.c brute.c
 	gcc -std=c99 -D_XOPEN_SOURCE=500 -Wall -pthread brute.c md5.c -O2 -o brute
 
+bench: md5.c bench.c
+	gcc -std=c99 -D_XOPEN_SOURCE=500 -Wall -pthread bench.c md5.c -O2 -o bench
+
 debug: md5.c brute.c
 	gcc -ggdb -std=c99 -D_XOPEN_SOURCE=500 -pthread -Wall brute.c md5.c -o brute
---
+++ bench.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <pthread.h>
+#include <sys/time.h> 
+#include "md5.h"
+
+void* bench(void* iters) {
+    const intptr_t iterations = (intptr_t)iters;
+    char* buffer = malloc(sizeof(char) * 64);
+    sprintf(buffer, "%d", rand());
+    buffer = md5(buffer);
+    for (int i = 0; i < iterations; i++)
+    {
+        char* old_buffer = buffer;
+        buffer = md5(buffer);
+        int len = strlen(buffer);
+        for (int i = 0; i < len; i++){
+            if (old_buffer[i] != buffer[i]) {
+                break;
+            }
+        }
+        for (int i = len-1; i > 0; i--){
+            if (old_buffer[i] != buffer[i]) {
+                break;
+            }
+        }
+        free(old_buffer);
+    }
+}
+
+int main() {
+    srand(time(0));
+    const int CPU_COUNT = get_nprocs();
+
+    printf("[bench->INFO] Running on 1 thread\n");
+    
+    struct timeval st_start, st_end;
+    gettimeofday(&st_start, NULL);
+    bench((void*) 10000000);
+    gettimeofday(&st_end, NULL);
+    
+    printf("[bench->INFO] Single threaded test took %lds%ldms\n", 
+        (st_end.tv_sec - st_start.tv_sec),
+        (((st_end.tv_sec*1000000+st_end.tv_usec)-(st_start.tv_sec*1000000+st_start.tv_usec))/1000)%1000);
+
+    printf("[bench->INFO] Running on %d threads\n", CPU_COUNT);
+    struct timeval mt_start, mt_end;
+    gettimeofday(&mt_start, NULL);
+    pthread_t threads[CPU_COUNT];
+    for (intptr_t i = 0; i < CPU_COUNT; i++) {
+        pthread_create(threads + i, NULL, bench, (void*)(10000000/CPU_COUNT));
+    }
+    for (intptr_t i = 0; i < CPU_COUNT; i++) {
+        pthread_join(threads[i], NULL);
+    }
+    gettimeofday(&mt_end, NULL);
+    printf("[bench->INFO] Multithreaded test took %lds%ldms\n", 
+        (mt_end.tv_sec - mt_start.tv_sec),
+        (((mt_end.tv_sec*1000000+mt_end.tv_usec)-(mt_start.tv_sec*1000000+mt_start.tv_usec))/1000)%1000);
+
+    return 0;
+}
